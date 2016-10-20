a0b6755a5806419fd5a24ff0becb68e2f013c02f
AmadoDomper <amado.domper@gmail.com>
2016-10-19 23:31:38 -0500

    Se creo la clase RolLN

---
+++ CHUYAChuya/CHUYAChuya.LogicaNegocio/RolLN.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHUYAChuya.AccesoDatos;
+using CHUYAChuya.EntidadesNegocio;
+
+namespace CHUYAChuya.LogicaNegocio
+{
+    public class RolLN
+    {
+        RolAD oRolAD;
+
+        public RolLN()
+        {
+            oRolAD = new RolAD();
+        }
+
+        public List<Rol> ListarRoles()
+        {
+            return oRolAD.ListarRoles();
+        }
+
+    }
+}
